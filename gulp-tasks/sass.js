3a4f075e5afb79181a8fefab0303a88ec948d006
blackdynamo <d16345401@gmail.com>
2016-01-19 09:12:34 -0700

    fixing the sass task. it was running at start and not returning a function to be executed whenever the task was executed.

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 
 
-module.exports = (options) => {
+module.exports = (options) => () => {
   gulp.src(options.paths)
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(options.build.css.dir));
